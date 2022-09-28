208625ed6a3ef629dc59c7679bec7f73a90404ef
Marks Polakovs <marks@markspolakovs.me>
2022-09-28 19:25:51 +0100

    initial commit

---
+++ _tools/update-stack.py
@@ -0,0 +1,22 @@
+import yaml
+import sys
+import os
+
+if len(sys.argv) < 5:
+    print("Usage: update-stack.py <stack-name> <environment-name> <image> <tag>")
+    sys.exit(1)
+
+_, STACK, ENV, IMG, TAG = sys.argv
+
+compose_file = f"{STACK}/{ENV}-docker-compose.yml"
+with open(compose_file, "r") as fd:
+    compose = yaml.load(fd, yaml.SafeLoader)
+
+for svc, data in compose["services"].items():
+    if "image" in data and IMG in data["image"]:
+        data["image"] = f"{IMG}:{TAG}"
+
+with open(compose_file, "w") as fd:
+    yaml.dump(compose, fd)
+
+print(f"Updated {compose_file}.")
